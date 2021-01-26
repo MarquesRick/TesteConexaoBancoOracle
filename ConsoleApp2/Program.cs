a8e5ee2d55bca94d63ebb16b9c4adde4204f8855
Henrique Marques <dev.sistemas@allis.com.br>
2021-01-26 00:02:46 -0300

    first commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Data;
+using Oracle.ManagedDataAccess.Client;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Connect
+            //string constr = "User Id=grails;Password=ora16ALLIS#grails;Data Source=10.200.203.13:1521/fpwprd";
+            string constr = "User Id=grails;Password=grailsnew;Data Source=10.200.203.13:1521/fpwhom";
+            OracleConnection con = new OracleConnection(constr);
+            con.Open();
+
+            // Execute a SQL SELECT
+            OracleCommand cmd = con.CreateCommand();
+            cmd.CommandText = "select email from usuario";
+            OracleDataReader reader = cmd.ExecuteReader();
+
+            // Print all employee numbers
+            while (reader.Read())
+                Console.WriteLine(reader.GetString(0));
+
+            // Clean up
+            reader.Dispose();
+            cmd.Dispose();
+            con.Dispose();
+        }
+    }
+}
